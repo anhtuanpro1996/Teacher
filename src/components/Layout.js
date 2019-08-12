29c75e343610d86b475d40ec3bff96d25bde0628
Lâm Phan Đình <lampd@topica.edu.vn>
2019-08-12 23:51:07 +0700

    update version new

---
+++ src/components/Layout.js
@@ -0,0 +1,35 @@
+
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import { Layout, Menu, Icon, Row, Col } from 'antd';
+import NavLeft from './NavLeft'
+const { Header, Sider, Footer, Content } = Layout;
+import 'antd/dist/antd.css';
+
+const divStyle = {
+	background: '#f9fafc',
+	padding: '32px 0px 0px',
+	height: "100%"
+};
+
+const LayoutMain = ({ title, children }) => (
+	<Row>
+		<Col span={20} offset={2} style={divStyle}>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<title>{title}</title>
+			<NavLeft />
+			<Layout style={{ background: '#f9fafc' }}>
+				{/* <Header style={{ background: '#fff', padding: 0 }} /> */}
+				<Content style={{ background: '#f9fafc' }}>
+					{children}
+				</Content>
+			</Layout>
+		</Col>
+	</Row>
+);
+
+LayoutMain.propTypes = {
+	title: PropTypes.string.isRequired,
+	children: PropTypes.node.isRequired,
+};
+export default LayoutMain;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import 'todomvc-app-css/index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, compose } from 'redux';
 import { Provider } from 'react-redux'
 import TodoApp from './containers/TodoApp';
 import rootReducer from './reducers/rootReducer';
-
+import LayoutMain from './components/Layout';
+import './index.css';
+import 'antd/dist/antd.css';
+import {I18nextProvider} from 'react-i18next';
+import i18next from 'i18next';
+import './i18n';
 // Dev tool
 import DevTools from './containers/DevTools';
 import { persistState } from 'redux-devtools';
+import {BrowserRouter as Router, Switch,Route} from 'react-router-dom';
+i18next.init({
+  interpolation: { escapeValue: false },  // React already does escaping
+});
+
 const enhancer = compose(
 	DevTools.instrument(),
 	persistState(
@@ -26,10 +36,17 @@ const store = createStore(rootReducer, initialState, enhancer);
 
 const appRoot = (
 	<Provider store={store}>
-		<div>
-			<TodoApp />
-			<DevTools />
-		</div>
+		<I18nextProvider i18n={i18next}>
+			<Router>
+				<div className="App">
+					<LayoutMain>
+						{/* <TodoApp /> */}
+						{/* <DevTools /> */}
+					</LayoutMain>
+				</div>
+			</Router>
+		</I18nextProvider>
+		
 	</Provider>
 )
 
