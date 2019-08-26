05e2c436136bed5161a20e2984727c1989c5acbb
QuanBH <quanbh@topica.edu.vn>
2019-08-26 19:26:47 +0700

    add context detail

--- src/components/Context/ContextMenu.js
+++ src/components/Context/ContextMenu.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './ContextMenu.css';
+import ImageDetail from './ImageDetail';
 
 const moveTo = {
   width: '100px',
@@ -7,14 +8,26 @@ const moveTo = {
   backgroundColor: 'blue',
 };
 export default function ContextMenu(props) {
-  // console.log('ContextMenu', props);
+  console.log('ContextMenu', props);
 
   const [moveToValue, movedHandle] = useState(false);
   const [hiddenContext, clickHandle] = useState(false);
+  const [visible, showModal] = useState(false);
+
+  const detailClicked = () => {
+    showModal(true);
+    clickHandle(true);
+  };
 
   const moveToClick = () => {
     movedHandle(true);
     clickHandle(true);
+  };
+
+  const renderDetailModal = () => {
+    return (
+      <ImageDetail visible={visible} dataRender={props.dataContext}/>
+    );
   };
 
   const renderMoveTo = (condition) => {
@@ -32,7 +45,7 @@ export default function ContextMenu(props) {
             <div className="itemContext" onClick={() => moveToClick(moveToValue)}>Di Chuyển tới</div>
             <div className="itemContext">Đổi tên</div>
             <div className="itemContext">Sao chép</div>
-            <div className="itemContext">Chi tiết</div>
+            <div className="itemContext" onClick={() => detailClicked()}>Chi tiết</div>
             <div className="itemContext">Tải xuống</div>
             <div className="itemContext">Xóa</div>
           </div>
@@ -51,6 +64,7 @@ export default function ContextMenu(props) {
     <React.Fragment>
       {hiddenContext ? '' : initContexMenu()}
       {renderMoveTo(moveToValue)}
+      {renderDetailModal()}
     </React.Fragment>
   );
 }
