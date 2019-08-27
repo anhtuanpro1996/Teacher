aae4ff8053d950ce6a73241516e69c72d5daae28
QuanBH <quanbh@topica.edu.vn>
2019-08-27 15:49:22 +0700

    remove comment

--- src/actions/thunks/fileContextAction.js
+++ src/actions/thunks/fileContextAction.js
@@ -1,35 +1,4 @@
-// export const createCourseInfo = ({data}) => {
-//   return (dispatch) => {
-//     return axios.post(`${URL.CREATE_COURSES_INFO}`, data)
-//       .then(response => {
-//         dispatch(createCourseInfoSuccess(response.data));
-//       })
-//       .catch(error => {
-//         throw (error);
-//       });
-//   };
-// };
 import axios from 'axios';
-// export function changeNameFile(fileID, data) {
-//   const url = 'http://157.230.255.33:8890/api/lms/files/' + fileID;
-//   console.log('changeNameFile', data);
-//   const dataSend = {name: data};
-//   return dispatch => {
-//     fetch(url, {
-//       method: 'PUT',
-//       body: JSON.stringify(dataSend),
-//     })
-//       .then(res => res.json())
-//       .then(res => {
-//         dispatch({ type: 'CHANGE_FILE_NAME_SUCCESS', payload: res});
-//         // console.log('rest', res);
-//       })
-//       .catch(error => {
-//         dispatch({ type: 'CHANGE_FILE_NAME_ERROR', payload: error});
-//       });
-//   };
-// };
-
 export function changeNameFile(fileID, data) {
   const url = 'http://157.230.255.33:8890/api/lms/files/' + fileID;
   const dataSend = {name: data};
