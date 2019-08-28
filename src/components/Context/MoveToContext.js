ca8dfe40bc4ab1850eba79f7d8fe99373a4ee2c3
QuanBH <quanbh@topica.edu.vn>
2019-08-28 19:03:20 +0700

    intergrated copy file and fix some bug reload

---
+++ src/components/Context/MoveToContext.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MoveToContext.css';
+
+const MoveToContext = () => {
+  return (
+    <div className="move-to-block">
+      <div className="header">header</div>
+      <div className="body">body</div>
+      <div className="footer">footer</div>
+    </div>
+  );
+};
+
+export default MoveToContext;
