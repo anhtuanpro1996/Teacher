ebbf74f3c1a9fc8075d50ef3c97094c48db216ea
QuanBH <quanbh@topica.edu.vn>
2019-08-29 02:02:34 +0700

    add theme move to fuction

--- src/components/Context/MoveToContext.js
+++ src/components/Context/MoveToContext.js
@@ -4,9 +4,31 @@ import './MoveToContext.css';
 const MoveToContext = () => {
   return (
     <div className="move-to-block">
-      <div className="header">header</div>
-      <div className="body">body</div>
-      <div className="footer">footer</div>
+      <div className="header">
+        <div className="header-title">
+          <div className="arrow-left"/>
+          <div className="title">Di chuyển tới</div>
+        </div>
+        <div className="close-modal"/>
+      </div>
+      <div className="body">
+        {[1, 2, 3, 4, 5].map((value, index) => {
+          return (
+            <div key={index} className="folder-element">
+              <div className="title-folder">
+                <div className="icon"/>
+                <div className="name">Thanhnv5.09912</div>
+              </div>
+              <div className="arrow-right"/>
+            </div>
+          );
+        })}
+      </div>
+      <div className="footer">
+        <div className="move-btn">
+          <div className="title">Di chuyển đến đây</div>
+        </div>
+      </div>
     </div>
   );
 };
