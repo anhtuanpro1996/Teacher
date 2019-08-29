f68ceb275924a194bd4af59d30047141783690a5
QuanBH <quanbh@topica.edu.vn>
2019-08-29 18:53:02 +0700

    fix cf

--- src/constants/Url.js
+++ src/constants/Url.js
@@ -4,7 +4,6 @@ const BASE_URL = 'http://157.230.255.33:8890/api/lms/';
 export const GET_ALL_USER = BASE_URL + 'users';
 export const SHOW_MARKED = 'show_marked';
 export const SHOW_UNMARKED = 'show_unmarked';
-export const CREATE_COURSES_INFO = 'http://localhost:4000/courses';
 export const GET_CHILD_FOLDER = BASE_URL + 'folders/';
 export const UPLOAD_FILE = BASE_URL + 'files';
 export const CATEGORY = BASE_URL + 'category';
